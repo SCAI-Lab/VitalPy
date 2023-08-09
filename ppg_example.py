9c926c6f78f12394f46022a930a78202368c9137
scidex <noyes7711+github@gmail.com>
2023-08-09 16:15:05 +0200

    Default file directory

--- ppg_example.py
+++ ppg_example.py
@@ -5,7 +5,7 @@ from src.ppg.PPGSignal import PPGSignal
 DATA_DIR = 'CHANGE'
 
 # Load an example signal from the AuroraBP database
-waveform_df = pd.read_csv(os.path.join(DATA_DIR, '/measurements_oscillometric/o000.initial.Sitting_arm_down.tsv'), delimiter='\t')
+waveform_df = pd.read_csv(os.path.join(DATA_DIR, '/measurements_oscillometric/o000/o000.initial.Sitting_arm_down.tsv'), delimiter='\t')
 waveform_df = waveform_df.rename(columns={'optical': 'ppg', 'ekg': 'ecg'})
 
 # The dataframe should have the columns 't' for time and 'ppg' for the ppg signal values.
